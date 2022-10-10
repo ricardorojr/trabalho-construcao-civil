a1408ad108a1451f838a28e5c723dc591ea142d5
Ricardo Rodrigues de Oliveira Júnior <ricardorodriguesjr87@gmail.com>
2022-10-10 19:14:31 -0300

    implementando api construcao-civil-rest

---
+++ construcao-civil-rest/src/test/java/com/construcao/construcaocivil/ConstrucaoCivilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.construcao.construcaocivil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConstrucaoCivilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
